2a2e3270d5c75ade95e84173ac59d863d58bbcb9
akhet <akhet@kolesa.kz>
2018-03-20 22:21:59 +0600

    Реализация парсера

--- launchnbc.py
+++ launchnbc.py
@@ -1,1 +1,22 @@
-#it is the very beginning
+from nltk.tokenize import RegexpTokenizer
+
+tokenizer = RegexpTokenizer(r'\w+')
+
+spams = []
+hams  = []
+
+def parse_input_dataset(spams, hams, filename):
+    f = open(filename, 'r', encoding="utf8")
+    for line in f:
+        if line[0] == 's':
+            spams.append(line[4:])
+        else:
+            hams.append(line[3:])
+
+
+parse_input_dataset(spams, hams, 'SMSSpamCollection')
+print(len(spams))
+print(len(hams))
+
+import sys
+sys.exit()
